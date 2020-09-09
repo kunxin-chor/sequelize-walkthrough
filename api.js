823ce6dcecab30182d4f45dbb3f2dc463a75f78b
kunxin-chor <chorkunxin@yahoo.com>
2020-09-09 11:11:12 +0000

    Added add product to tag

--- api.js
+++ api.js
@@ -22,8 +22,29 @@ router.get('/products/tags/:tagid', async (req,res)=>{
     res.send(results.map(r=>r.toJSON()))
 })
 
+router.post('/products/tag/:name', async (req, res)=>{
+    let product = await ProductModel.create({
+        'name': req.body.productName,
+        'quantity': req.body.quantity,
+        'productCode': req.body.productCode,        
+    })
+
+    // find the tag by its name or create one if it doesn't exist
+    let tag = await TagModel.findOrCreate({
+        where: {
+            name: req.params.name
+        }
+    })
+
+    product.addTag(tag);
+
+    // if your relationship is between teacher and class
+    // teacher.addClass(theClass)
+
+
+})
+
 router.post('/products', async(req,res)=>{
-    console.log(req.body);
     let productName = req.body.productName;
     let quantity = req.body.quantity;
     let productCode = req.body.productCode;
